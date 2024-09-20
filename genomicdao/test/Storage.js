c8a6fb1c335dd8618d7141df169641d48148d38e
Tran Minh Quy <minhquylt95@gmail.com>
2024-09-20 14:28:00 +0700

    TASK-3: implement Access Control mechanism (#3)
    * TASK-1: fill todo to pass testcase
    
    * TASK-1: declare and emit events: MintedGNFT, RewardedPCSP
    
    * TASK-2: create /scripts/deploy.js for deploying
    
    * TASK-3: implement Access Control mechanism

--- genomicdao/test/Storage.js
+++ genomicdao/test/Storage.js
@@ -171,4 +171,83 @@ describe("Controller", function () {
       ).to.be.revertedWith("Session is ended")
     })
   })
+
+  async function deployControllerThenConfirm() {
+    const {controller, addr1, addr2} = await loadFixture(deployControllerFixture);
+
+    const docId = "doc1"
+    const contentHash = "dochash"
+    const proof = "success"
+    const riskScore = 1
+    const sessionId = 0
+
+    await controller.uploadData(docId)
+    const nft = await controller.confirm(docId, contentHash, proof, sessionId, riskScore)
+    const nftId = nft.value
+    return {controller, addr1, addr2, nftId, docId, contentHash}
+  }
+
+  describe("Grant Access", function () {
+    it("should allow NFT owner to grant access to another address and emit GrantedAccess event", async function () {
+      const { controller, addr1, nftId, docId, contentHash } = await loadFixture(deployControllerThenConfirm);
+
+
+      await controller.grantAccess(nftId, addr1.address);
+
+      const hasAccess = await controller.connect(addr1).requestAccess(nftId);
+      expect(hasAccess.length).to.be.equal(2);
+      expect(hasAccess[0]).to.be.equal(docId);
+      expect(hasAccess[1]).to.be.equal(contentHash);
+
+      await expect(controller.grantAccess(nftId, addr1.address))
+        .to.emit(controller, "GrantedAccess")
+        .withArgs(nftId, addr1.address);
+    });
+
+    it("should not allow non-owner to grant access", async function () {
+      const {controller, addr1, nftId, addr2} = await loadFixture(deployControllerThenConfirm);
+
+      await expect(
+        controller.connect(addr1).grantAccess(nftId, addr2.address)
+      ).to.be.revertedWith("You are not the owner of this NFT.");
+    });
+  });
+
+  describe("Revoke Access", function () {
+    it("should allow NFT owner to revoke access from another address and emit RevokedAccess event", async function () {
+      const { controller, addr1, nftId, docId, contentHash } = await loadFixture(deployControllerThenConfirm);
+
+      // step 1: Grant Access -> check requestAccess and event
+      await controller.grantAccess(nftId, addr1.address);
+
+      const dataDoc = await controller.connect(addr1).requestAccess(nftId);
+      expect(dataDoc.length).to.be.equal(2);
+      expect(dataDoc[0]).to.be.equal(docId);
+      expect(dataDoc[1]).to.be.equal(contentHash);
+
+      await expect(controller.grantAccess(nftId, addr1.address))
+        .to.emit(controller, "GrantedAccess")
+        .withArgs(nftId, addr1.address);
+
+      // step 2: Revoke Access -> check event and requestAccess
+      await controller.revokeAccess(nftId, addr1.address);
+
+      await expect(controller.revokeAccess(nftId, addr1.address))
+        .to.emit(controller, "RevokedAccess")
+        .withArgs(nftId, addr1.address);
+
+      await expect(
+        controller.connect(addr1).requestAccess(nftId)
+      ).to.be.revertedWith("Access denied.");
+    });
+
+    it("should not allow non-owner to revoke access", async function () {
+      const {controller, addr1, nftId, addr2} = await loadFixture(deployControllerThenConfirm);
+
+      await expect(
+        controller.connect(addr1).revokeAccess(nftId, addr2.address)
+      ).to.be.revertedWith("You are not the owner of this NFT.");
+    });
+  });
+
 })
